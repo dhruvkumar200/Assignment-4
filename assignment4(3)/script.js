3d99873650ac098c5a5b7c57e319a4360bceb2b9
dhruvkumar200 <123928947+dhruvkumar200@users.noreply.github.com>
2023-02-13 13:53:08 +0530

    Delete assignment4(3) directory

--- assignment4(3)/script.js
+++
@@ -1,8 +0,0 @@
-function moveButton(elem){
-    if( $(elem).parent().attr("id") == "container1" ){
-        $(elem).detach().appendTo('#container2');
-    }
-    else{
-        $(elem).detach().appendTo('#container1'); 
-    }
-}
